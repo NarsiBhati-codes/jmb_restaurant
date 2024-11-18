ef460703baaf17c82881917a1265351596111af9
NarsiBhatii <narsibhati31102@gmail.com>
2024-11-19 01:51:04 +0530

    routes added

---
+++ app/location/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function page() {
+  return (
+    <div>
+      Location
+      <h1>
+        <a href="http://localhost:3000">Home</a>
+      </h1>
+    </div>
+  );
+}
+
+export default page;
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,21 @@
 export default function Home() {
-  return <div>Jai Maa Bhawani Dal Bati Churma</div>;
+  return (
+    <div>
+      <h1>
+        <a href="http://localhost:3000/blog">blog</a>
+      </h1>
+      <h1>
+        <a href="http://localhost:3000/location">location</a>
+      </h1>
+      <h1>
+        <a href="http://localhost:3000/contactus">contactus</a>
+      </h1>
+      <h1>
+        <a href="http://localhost:3000/menu">menu</a>
+      </h1>
+      <h1>
+        <a href="http://localhost:3000/faq">FAQ</a>
+      </h1>
+    </div>
+  );
 }
